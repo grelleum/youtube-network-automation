33b5f7dc4cee13fe49cc96f9bdb5456b9d1294a8
Greg Mueller <greg.p.mueller@gmail.com>
2017-05-12 20:47:07 -0400

    07.JSON_and_getpass

---
+++ 07.JSON_and_getpass/v3.use_getpass/cmdrunner.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+
+from getpass import getpass
+import json
+import netmiko
+
+def get_input(prompt=''):
+    try:
+        line = raw_input(prompt)
+    except NameError:
+        line = input(prompt)
+    return line
+
+def get_credentials():
+    """Prompt for and return a username and password."""
+    username = get_input('Enter Username: ')
+    password = None
+    while not password:
+        password = getpass()
+        password_verify = getpass('Retype your password: ')
+        if password != password_verify:
+            print('Passwords do not match.  Try again.')
+            password = None
+    return username, password
+
+netmiko_exceptions = (netmiko.ssh_exception.NetMikoTimeoutException,
+                      netmiko.ssh_exception.NetMikoAuthenticationException)
+
+username, password = get_credentials()
+
+with open('devices.json') as dev_file:
+     devices = json.load(dev_file)
+
+for device in devices:
+    device['username'] = username
+    device['password'] = password
+    try:
+        print('~' * 79)
+        print('Connecting to device:', device['ip'])
+        connection = netmiko.ConnectHandler(**device)
+        print(connection.send_command('show clock'))
+        connection.disconnect()
+    except netmiko_exceptions as e:
+        print('Failed to ', device['ip'], e)
+    
---
+++ 07.JSON_and_getpass/v4.import_mytools/cmdrunner.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+
+import json
+import netmiko
+import mytools
+
+netmiko_exceptions = (netmiko.ssh_exception.NetMikoTimeoutException,
+                      netmiko.ssh_exception.NetMikoAuthenticationException)
+
+username, password = mytools.get_credentials()
+
+with open('devices.json') as dev_file:
+     devices = json.load(dev_file)
+
+for device in devices:
+    device['username'] = username
+    device['password'] = password
+    try:
+        print('~' * 79)
+        print('Connecting to device:', device['ip'])
+        connection = netmiko.ConnectHandler(**device)
+        print(connection.send_command('show clock'))
+        connection.disconnect()
+    except netmiko_exceptions as e:
+        print('Failed to ', device['ip'], e)
+    
